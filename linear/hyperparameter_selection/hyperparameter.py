79b7a126b335bec3dd779eb94d7e7809ce378107
Dmitry Vasilkov <113594620+DmitryVasilkovW@users.noreply.github.com>
2024-11-14 02:47:00 +0300

    refactor: add static class for params

--- linear/hyperparameter_selection/hyperparameter.py
+++ linear/hyperparameter_selection/hyperparameter.py
@@ -3,56 +3,110 @@ from linear.classifier.svm_classifier import SVM
 from linear.dataset.data import accuracy_metric
 
 
-def params(X_train, y_train, X_test, y_test):
+class HyperparameterSelection:
     best_accuracy_ridge = 0
     best_alpha_ridge = 0
-
-    # alphas_ridge = [0.01, 0.1, 1.0, 10.0]
-    alphas_ridge = [0.01, 0.3, 1.0, 10.0]
-    for alpha in alphas_ridge:
-        ridge = RidgeRegression(alpha=alpha)
-        ridge.fit(X_train, y_train)
-        y_pred = ridge.predict_classes(X_test)
-        accuracy = accuracy_metric(y_test, y_pred)
-        if accuracy > best_accuracy_ridge:
-            best_accuracy_ridge = accuracy
-            best_alpha_ridge = alpha
-
-    # Перебор гиперпараметров для Logistic Regression
     best_accuracy_logistic = 0
     best_alpha_logistic = 0
-
-    # alphas_logistic = [0.01, 0.1, 1.0]
-    alphas_logistic = [0.01, 0.3, 1.0]
-    for alpha in alphas_logistic:
-        log_reg = LogisticRegressionGD(alpha=alpha, iterations=1000, penalty='l2')
-        log_reg.fit(X_train, y_train)
-        y_pred = log_reg.predict_classes(X_test)
-        accuracy = accuracy_metric(y_test, y_pred)
-        if accuracy > best_accuracy_logistic:
-            best_accuracy_logistic = accuracy
-            best_alpha_logistic = alpha
-
-    # Перебор гиперпараметров для SVM
     best_accuracy_svm = 0
-    best_C_svm = 0
+    best_c_svm = 0
     best_kernel_svm = ''
-    kernels = ['linear', 'polynomial', 'rbf']
-    Cs = [0.1, 0.3]
 
-    for C in Cs:
-        for kernel in kernels:
-            svm = SVM(C=C, alpha=0.01, kernel=kernel)
-            svm.fit(X_train, 2 * y_train - 1)
-            y_pred = svm.predict(X_test)
-            accuracy = accuracy_metric(y_test, (y_pred >= 0).astype(int))
-            if accuracy > best_accuracy_svm:
-                best_accuracy_svm = accuracy
-                best_C_svm = C
-                best_kernel_svm = kernel
+    @classmethod
+    def _set_hyperparameters(cls, x_train, y_train, x_test, y_test):
+        cls._clean()
+        alphas_ridge = [0.01, 0.3, 1.0]
+        for alpha in alphas_ridge:
+            ridge = RidgeRegression(alpha=alpha)
+            ridge.fit(x_train, y_train)
+            y_pred = ridge.predict_classes(x_test)
+            accuracy = accuracy_metric(y_test, y_pred)
+            if accuracy > cls.best_accuracy_ridge:
+                cls.best_accuracy_ridge = accuracy
+                cls.best_alpha_ridge = alpha
 
-    print(f'Best alpha for Ridge Regression: {best_alpha_ridge}, Best accuracy: {best_accuracy_ridge:.2f}')
-    print(f'Best alpha for Logistic Regression: {best_alpha_logistic}, Best accuracy: {best_accuracy_logistic:.2f}')
-    print(f'Best C for SVM: {best_C_svm}, Best kernel: {best_kernel_svm}, Best accuracy: {best_accuracy_svm:.2f}')
+        alphas_logistic = [0.01, 1.0]
+        for alpha in alphas_logistic:
+            log_reg = LogisticRegressionGD(alpha=alpha, iterations=1000, penalty='l2')
+            log_reg.fit(x_train, y_train)
+            y_pred = log_reg.predict_classes(x_test)
+            accuracy = accuracy_metric(y_test, y_pred)
+            if accuracy > cls.best_accuracy_logistic:
+                cls.best_accuracy_logistic = accuracy
+                cls.best_alpha_logistic = alpha
 
-    return best_accuracy_ridge, best_alpha_ridge, best_accuracy_logistic, best_alpha_logistic, best_C_svm, best_kernel_svm
+        kernels = ['linear', 'polynomial', 'rbf']
+        cs = [0.1, 0.3]
+
+        for C in cs:
+            for kernel in kernels:
+                svm = SVM(C=C, alpha=0.01, kernel=kernel)
+                svm.fit(x_train, 2 * y_train - 1)
+                y_pred = svm.predict(x_test)
+                accuracy = accuracy_metric(y_test, (y_pred >= 0).astype(int))
+                if accuracy > cls.best_accuracy_svm:
+                    cls.best_accuracy_svm = accuracy
+                    cls.best_c_svm = C
+                    cls.best_kernel_svm = kernel
+
+        print(
+            f'Best alpha for Ridge Regression: {cls.best_alpha_ridge},'
+            f' Best accuracy: {cls.best_accuracy_ridge:.2f}')
+        print(
+            f'Best alpha for Logistic Regression: {cls.best_alpha_logistic},'
+            f' Best accuracy: {cls.best_accuracy_logistic:.2f}')
+        print(
+            f'Best C for SVM: {cls.best_c_svm}, Best kernel: {cls.best_kernel_svm},'
+            f' Best accuracy: {cls.best_accuracy_svm:.2f}')
+
+    @classmethod
+    def _clean(cls):
+        cls.best_accuracy_ridge = 0
+        cls.best_alpha_ridge = 0
+        cls.best_accuracy_logistic = 0
+        cls.best_alpha_logistic = 0
+        cls.best_accuracy_svm = 0
+        cls.best_c_svm = 0
+        cls.best_kernel_svm = ''
+
+    @classmethod
+    def get_best_accuracy_ridge(cls, x_train, y_train, x_test, y_test):
+        if cls.best_accuracy_ridge == 0:
+            cls._set_hyperparameters(x_train, y_train, x_test, y_test)
+        return cls.best_accuracy_ridge
+
+    @classmethod
+    def get_best_alpha_ridge(cls, x_train, y_train, x_test, y_test):
+        if cls.best_alpha_ridge == 0:
+            cls._set_hyperparameters(x_train, y_train, x_test, y_test)
+        return cls.best_alpha_ridge
+
+    @classmethod
+    def get_best_accuracy_logistic(cls, x_train, y_train, x_test, y_test):
+        if cls.best_accuracy_logistic == 0:
+            cls._set_hyperparameters(x_train, y_train, x_test, y_test)
+        return cls.best_accuracy_logistic
+
+    @classmethod
+    def get_best_alpha_logistic(cls, x_train, y_train, x_test, y_test):
+        if cls.best_alpha_logistic == 0:
+            cls._set_hyperparameters(x_train, y_train, x_test, y_test)
+        return cls.best_alpha_logistic
+
+    @classmethod
+    def get_best_accuracy_svm(cls, x_train, y_train, x_test, y_test):
+        if cls.best_accuracy_svm == 0:
+            cls._set_hyperparameters(x_train, y_train, x_test, y_test)
+        return cls.best_accuracy_svm
+
+    @classmethod
+    def get_best_c_svm(cls, x_train, y_train, x_test, y_test):
+        if cls.best_c_svm == 0:
+            cls._set_hyperparameters(x_train, y_train, x_test, y_test)
+        return cls.best_c_svm
+
+    @classmethod
+    def get_best_kernel_svm(cls, x_train, y_train, x_test, y_test):
+        if cls.best_kernel_svm == '':
+            cls._set_hyperparameters(x_train, y_train, x_test, y_test)
+        return cls.best_kernel_svm
--- linear/visualisation/main.py
+++ linear/visualisation/main.py
@@ -1,18 +1,18 @@
 from linear.dataset.data import get_data
 from linear.dataset.select_data import DataProcessor
-from linear.hyperparameter_selection.hyperparameter import params
+from linear.hyperparameter_selection.hyperparameter import HyperparameterSelection
 from linear.utils.training_utils import tmp
 
 data = get_data()
 repo = DataProcessor(data, "Class")
-X_train = repo.get("x", "train")
-X_test = repo.get("x", "test")
+x_train = repo.get("x", "train")
+x_test = repo.get("x", "test")
 y_train = repo.get("y", "train")
 y_test = repo.get("y", "test")
 
-best_accuracy_ridge, best_alpha_ridge, best_accuracy_logistic, best_alpha_logistic, best_C_svm, best_kernel_svm = params(
-    X_train,
-    y_train, X_test,
-    y_test)
+best_alpha_logistic = HyperparameterSelection.get_best_alpha_logistic(x_train, y_train, x_test, y_test)
+best_c_svm = HyperparameterSelection.get_best_c_svm(x_train, y_train, x_test, y_test)
+best_kernel_svm = HyperparameterSelection.get_best_kernel_svm(x_train, y_train, x_test, y_test)
+best_alpha_ridge = HyperparameterSelection.get_best_alpha_ridge(x_train, y_train, x_test, y_test)
 
-tmp(X_train, y_train, X_test, y_test, best_alpha_logistic, best_C_svm, best_kernel_svm, best_alpha_ridge)
+tmp(x_train, y_train, x_test, y_test, best_alpha_logistic, best_c_svm, best_kernel_svm, best_alpha_ridge)
